40d2e6a055891c4ccfe3f9b41d5b3072b5c486a8
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-01-28 12:13:48 +0100

    Set up the node server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+
+const app = express();  
+
+const port = process.env.PORT || 3002;
+
+app.listen(port , () => {
+    console.log(`Server running at ${port}`);
+})
