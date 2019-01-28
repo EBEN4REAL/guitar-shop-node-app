9f42af4560e88a9a1b1766d6ac00106c0fa92d0d
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-01-28 16:46:53 +0100

    completed the hashing of password feature

--- server/models/user.js
+++ server/models/user.js
@@ -43,15 +43,20 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save', function(next){
     var user = this;
 
-    bcrypt.genSalt(SALT, function(err, salt){
-        if(err) return next(err);
+    if(user.isModified('password')){  // IF They are changing the passwords
+        bcrypt.genSalt(SALT, function (err, salt) {
+            if (err) return next(err);
 
-        bcrypt.hash(user.password, salt, function(err, hash){
-            if(err) return next(err);
-            user.password = hash;
-            next();
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            })
         })
-    })
+    }
+    else{
+        next();
+    }
 })
 
 const User = mongoose.model('User', userSchema);
