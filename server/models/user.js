0f2c843e9a19667a9ff65743d37c2029bdb2f025
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-01-28 18:14:36 +0100

    completed the hashing of password feature

--- server/models/user.js
+++ server/models/user.js
@@ -59,8 +59,12 @@ userSchema.pre('save', function(next){
     }
 })
 
-userSchema.methods.comparePassword = (candidatePassword, callback) => {
-    
+userSchema.methods.comparePassword = function(candidatePassword, callback){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if(err) return callback(err);
+        callback(null, isMatch);
+
+    });
 }
 
 const User = mongoose.model('User', userSchema);
