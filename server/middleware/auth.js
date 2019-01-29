7429fc56c7baa841ce30573a0025dcad38c05244
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-01-29 19:52:58 +0100

    added verify tokem method

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,11 @@ let auth = (req,res,next) => {
     let token = req.cookies.w_auth;
 
     User.findByToken(token, (err,user) => {
-        
+        if(err) throw err;
+        if(!user) return res.json({
+            isAuth: false,
+            error: true
+        })
     })
 }
 
