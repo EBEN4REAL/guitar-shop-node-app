3f630d62ced363c65b3876abd2b5c698a6d17648
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-03-26 16:20:20 +0100

    started the add product section

--- client/src/components/User/Admin/add_product.js
+++ client/src/components/User/Admin/add_product.js
@@ -1,12 +1,22 @@
 import React, {Component}  from 'react';
+import UserLayout from '../../../hoc/user';
+import FormField from '../../utils/Form/FormField';
+import {update , generateData, isFormValid} from '../../utils/Form/FormActions';
 
-class Card extends Component {
+import {connect} from 'react-redux';
+
+
+class AddProduct extends Component {
     render(){
         return (
             <div>
-
+                <UserLayout>
+                    <div>
+                        Add product.
+                    </div>
+                </UserLayout>
             </div>
         )
     }
 }
-export default Card
+export default connect()(AddProduct);
--- client/src/routes.js
+++ client/src/routes.js
@@ -7,6 +7,7 @@ import Register from './components/Register_login/register';
 import UserDashboard from './components/User';
 import Auth from './hoc/auth'
 import Shop from './components/shop/shopIndex';
+import AddProdcut from '../src/components/User/Admin/add_product';
 
 
 
@@ -15,12 +16,13 @@ const Routes = () => {
     <Layout>
       <Switch>
         <Route path="/user/dashboard" exact component={Auth(UserDashboard, true)} />
+        <Route path="/admin/add_product" exact component={Auth(AddProdcut, true)} />
 
         <Route path="/register" exact component={Auth(Register, false)} />
         <Route path="/" exact component={Auth(Home, null)} />
         <Route path="/register_login" exact component={Auth(RegisterLogin, false)} />
         <Route path="/shop" exact component={Auth(Shop, null)} />
-        <Route path="/add_product" exact component={Auth()} />
+        
       </Switch>
     </Layout>
     
