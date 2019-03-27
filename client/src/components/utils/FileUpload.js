49930cc14d86978c2937be23108d35af2c5a128c
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-03-27 18:47:27 +0100

    successfully saving products into DB

---
+++ client/src/components/utils/FileUpload.js
@@ -0,0 +1,18 @@
+import React, {Component}  from 'react'
+import DropZone from 'react-dropzone';
+import axios from 'axios';
+
+import FontAwesomeIcon from '@fortawesome/fontawesome';
+import faPlusCircle from '@fortawesome/fontawesome-free-solid/faPlusCircle';
+import CircularProgress from '@material-ui/core';
+
+class FileUpload extends Component {
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default FileUpload
