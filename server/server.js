eb26459f75b70012dc679db3208390e85d2e1e07
Igbinoba Ebenezer <ebenezer.igbinoba@stu.cu.edu.ng>
2019-04-10 16:08:54 +0100

    Finished the upload picture  section

--- server/server.js
+++ server/server.js
@@ -32,9 +32,9 @@ app.use(cookieParser());
 
 
 cloudinary.config({
-    cloud_name: "stovix-limited",
-    api_key: "334321928551238",
-    api_secret: "Kucs_03bhKkdvVmBAxy9StA7MDc"
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
 })
 
 // MIDDLEWARES
